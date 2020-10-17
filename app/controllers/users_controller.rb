b6113bf94c8d3faf57b72a5bbc2f39b5692d61ec
Jose Abel <joseabel.ramirezfrontany@gmail.com>
2020-10-16 21:57:45 -0400

    Add user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,11 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def show
+    @user = User.find(params[:id])
+    @articles = @user.articles
+  end
+
   def edit
     @user = User.find(params[:id])
   end
