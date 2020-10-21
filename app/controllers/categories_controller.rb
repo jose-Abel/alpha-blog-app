cd773bc6714c2df1025b46089cd4480726c6880e
Jose Abel <joseabel.ramirezfrontany@gmail.com>
2020-10-21 12:29:09 -0400

    Add categories_controller. Add new and show templates for categories. Add categories_controller_test

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,11 +1,30 @@
 class CategoriesController < ApplicationController
   
   def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:notice] = "Category was successfully created"
+      redirect_to @category
+    else
+      render 'new'
+    end
   end
 
   def index
   end
 
   def show
+    @category = Category.find(params[:id])
   end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
+
 end
