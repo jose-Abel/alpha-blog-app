1a14610711840524fe898246d0c9910cb0844dbe
Jose Abel <joseabel.ramirezfrontany@gmail.com>
2020-10-21 14:00:23 -0400

    Add controller test to ensure categories cannot be created by non admins. Add require_admin method private to categories controller and use it as before_action for new and create actions.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,6 @@
 class CategoriesController < ApplicationController
-  
+  before_action :require_admin, except: [:index, :show]
+
   def new
     @category = Category.new
   end
@@ -15,6 +16,7 @@ class CategoriesController < ApplicationController
   end
 
   def index
+    @categories = Category.paginate(page: params[:page], per_page: 5)
   end
 
   def show
@@ -27,4 +29,10 @@ class CategoriesController < ApplicationController
     params.require(:category).permit(:name)
   end
 
+  def require_admin
+    if !(logged_in? && current_user.admin?)
+      flash[:alert] = "Only admins can perform that action"
+      redirect_to categories_path
+    end
+  end
 end
