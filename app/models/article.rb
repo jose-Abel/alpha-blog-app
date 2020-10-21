3091a4781823d882eb0d6fce7dae65b8a459c50a
Jose Abel <joseabel.ramirezfrontany@gmail.com>
2020-10-21 19:18:01 -0400

    Update the maximum for the description to 1000

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,5 +3,5 @@ class Article < ApplicationRecord
   has_many :article_categories
   has_many :categories, through: :article_categories
   validates :title, presence: true, length: { minimum: 6, maximum: 100 }
-  validates :description, presence: true, length: { minimum: 10, maximum: 500 }
+  validates :description, presence: true, length: { minimum: 10, maximum: 1000 }
 end
